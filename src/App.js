eb78db9c304a5cba0217713c1580c01fbc2aae92
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-03-31 19:46:23 +0530

    Reasons part Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Reasions from './components/Reasons/Reasions';
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasions />
     </div>
   );
 }
