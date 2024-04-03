965eee49d77b3b5808bb8cc581658c69aff3a1d0
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-04-03 21:16:37 +0530

    Footer Part is Completed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Footer.css';
+import GitHub from "../../assets/github.png";
+import Intagram from "../../assets/instagram.png"
+import Linkedin from '../../assets/linkedin.png'
+import Logo from "../../assets/logo.png"
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer-container">
+                <hr />
+                <div className='footer'>
+                    <div className="social">
+                        <img src={GitHub} alt="" />
+                        <img src={Intagram} alt="" />
+                        <img src={Linkedin} alt="" />
+                    </div>
+                    <div className="logo">
+                        <img src={Logo} alt="" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
